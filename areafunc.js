1331408022fc73d67b626f3c51b6b093dfbb07e7
Sohail-Effendi <160319647+Sohail-Effendi@users.noreply.github.com>
2024-05-12 10:57:11 +0500

    Calculate area function

---
+++ areafunc.js
@@ -0,0 +1,5 @@
+function areacalculate(width, height) {
+    return width * height;
+}
+var arearesult = areacalculate(20, 25);
+console.log(arearesult);
---
+++ areafunc.ts
@@ -0,0 +1,5 @@
+function areacalculate(width : number, height : number) : number {
+      return width * height
+}
+let arearesult = areacalculate (20, 25);
+console.log(arearesult);
